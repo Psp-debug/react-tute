7a8ffb03bda84b94b6a91caf95d32fce054580bd
Psp-debug <ps9925231677@gmail.com>
2024-02-22 12:59:07 +0530

    commit

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [color, setColor] = useState('olive')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='w-screen h-screen duration-200' 
+     style={{backgroundColor: color}}
+     >
+      <div className='fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2'>
+        <div className='flex felx-wrap justify-center bg-white shadow-lg gap-3 px-1 py-1 rounded-3xl'>
+          <button onClick={() => setColor('blue')} className=' rounded-full' style={{color: 'black', backgroundColor: 'blue'}}>Blue</button>
+          <button onClick={() => setColor('red')} className=' rounded-full' style={{color: 'black', backgroundColor: 'red'}}>Red</button>
+          <button onClick={() => setColor('green')} className=' rounded-full' style={{color: 'black', backgroundColor: 'green'}}>Green</button>
+          <button onClick={() => setColor('yellow')} className=' rounded-full' style={{color: 'black', backgroundColor: 'yellow'}}>Yellow</button>
+          <button onClick={() => setColor('pink')} className=' rounded-full' style={{color: 'black', backgroundColor: 'pink'}}>Pink</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </div>
+    </>   
   )
 }
 
